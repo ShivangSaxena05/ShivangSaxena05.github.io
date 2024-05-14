61cd23f144ad00feddfdc9c8961092cf2ab9ddc0
ShivangSaxena05 <82094331+ShivangSaxena05@users.noreply.github.com>
2024-05-14 07:44:26 +0530

    My Personal Portfolio

--- portfolio.js
+++
@@ -1,9 +0,0 @@
-function link1(){
-    window.location.href="https://www.instagram.com/vanshsaxena28"
-}
-function link2(){
-    window.location.href="https://www.linkedin.com/in/shivang-saxena-699974267/"
-}
-function link3(){
-    window.location.href="https://www.github.com/ShivangSaxena05"
-}
